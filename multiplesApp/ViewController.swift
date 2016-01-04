4ce42f56b6ddf3ba7b4aec5d2e066f50f4bf5e86
Jareedos <jaredallens@gmail.com>
2016-01-03 21:47:19 -0800

    added and layed out the assets

--- multiplesApp/ViewController.swift
+++ multiplesApp/ViewController.swift
@@ -9,17 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var multiplesImage: UIImageView!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var multiplesTxtField: UITextField!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var addLbl: UILabel!
+    
 
 }
 
